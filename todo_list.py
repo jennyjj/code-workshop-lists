70ddaa4d99963378f6723ff010dd1edf2a343765
Jenny <kang.je@gmail.com>
2017-04-24 23:33:41 -0700

    finished the program

--- todo_list.py
+++ todo_list.py
@@ -11,37 +11,34 @@ You can run the script in your Terminal at any time using the command:
 
 def add_to_list(my_list):
     """Takes user input and adds it as a new item to the end of the list."""
-
-    print "The add_to_list function has not yet been written"
-
+    item = raw_input("What would you like to add? ")
+    my_list.append(item)
 
 def view_list(my_list):
     """Print each item in the list."""
 
-    print "The view_list function has not yet been written"
+    for item in my_list:
+        print item
 
 
 def display_main_menu(my_list):
     """Displays main options, takes in user input, and calls view or add function."""
 
-    user_options = raw_input("Would you like to A. Add a new item, B. View list, C. Quit the program")
-
-
-    """
-    \nWould you like to:
-    A. Add a new item
-    B. View list
-    C. Quit the program
-    >>> """
-
+    user_options = "Would you like to: A. Add a new item, B. View list, C. Quit the program "
+ 
     while True:
-        if user_options == "A":
-            add_to_list()
-        elif user_options == "B":
-            view_list()
+        user_input = raw_input(user_options)
+        if user_input == "A":
+            add_to_list(my_list)
+        elif user_input == "B":
+            view_list(my_list)
         elif user_input == "C":
         # Collect input and include your if/elif/else statements here.
             break
+        else:
+            #inappropriate response
+            print "That is not an appropriate answer."
+            print "Please provide another response."
 
 #-------------------------------------------------
 
