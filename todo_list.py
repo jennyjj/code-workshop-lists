0adab75e48cca3ef4085ddf7608b294714f8bcac
Jenny <kang.je@gmail.com>
2017-04-24 22:32:13 -0700

    added some more code under display main menu

--- todo_list.py
+++ todo_list.py
@@ -24,7 +24,7 @@ def view_list(my_list):
 def display_main_menu(my_list):
     """Displays main options, takes in user input, and calls view or add function."""
 
-    user_options = raw_input("/nWould you like to A. Add a new item, B. View list, C. Quit the program")
+    user_options = raw_input("Would you like to A. Add a new item, B. View list, C. Quit the program")
 
 
     """
@@ -35,8 +35,13 @@ def display_main_menu(my_list):
     >>> """
 
     while True:
+        if user_options == "A":
+            add_to_list()
+        elif user_options == "B":
+            view_list()
+        elif user_input == "C":
         # Collect input and include your if/elif/else statements here.
-        break
+            break
 
 #-------------------------------------------------
 
